5723f22ed8c6607aec0213545ea10762776d8927
Akhyar Rahman <akhyarrh@users.noreply.github.com>
2020-12-07 04:22:04 +0000

    Add WhatsApp domain

--- src/background.js
+++ src/background.js
@@ -15,6 +15,7 @@ const FACEBOOK_DOMAINS = [
   "cdninstagram.com", "instagramstatic-a.akamaihd.net", "instagramstatic-a.akamaihd.net.edgesuite.net",
 
   "messenger.com", "m.me", "messengerdevelopers.com",
+  "whatsapp.com", "whatsapp.net",
 
   "atdmt.com",
 
